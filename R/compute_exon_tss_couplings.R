0f2afd127cd94fc1ffb0e7573afc67ee8207a6fb
cag1343 <carlos.alfonso.de@gmail.com>
2022-12-19 21:05:44 +0100

    SaiLoR v1

---
+++ R/compute_exon_tss_couplings.R
@@ -0,0 +1,181 @@
+#' Compute couplings exon junctions and Transcription start sites
+#'
+#' @param junctions: junction data with to junction-reads assignments
+#' @param read_ids: read_ids for full length reads
+#' @param pathJunctionRef: reference database used.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+compute_exon_tss_couplings <- function(junctions,read_ids,pathJunctionRef){
+  juncSums <- readr::read_tsv(junctions) %>% dplyr::select(new_junID, read_id)
+  fullLengths <- readr::read_tsv(read_ids)
+  junctionRef<- rtracklayer::import.gff(pathJunctionRef)
+  # keep only junctions in database
+  juncSums <- juncSums %>% filter(new_junID %in% junctionRef$juncID)
+  readsToJunctions <- left_join(fullLengths,
+                                juncSums %>% dplyr::rename(name = read_id),
+                                by = "name")
+  # summary junction combinations found.add less memory consuming id
+  tmpRefjun <- readsToJunctions %>%
+    distinct(new_junID, .keep_all = TRUE) %>%
+    arrange(new_junID) %>%
+    group_by(gene_id) %>%
+    mutate(jun_id = paste0("J", sequence(n()))) %>%
+    ungroup()
+  readsToJunctions <- left_join(
+    readsToJunctions,
+    tmpRefjun %>% dplyr::select(new_junID,jun_id),
+    by =  "new_junID"
+  )
+  readsToJunctionsCollapse <- readsToJunctions %>%
+    group_by(name) %>%
+    mutate(juncComb = paste0(jun_id, collapse = ",")) %>% mutate(juncComb=paste0(gene_id, ":", juncComb))
+  junCombCounts <- readsToJunctionsCollapse %>%
+    distinct(name , .keep_all = TRUE) %>%
+    group_by(juncComb) %>%
+    tally()
+  # keep only genes with more than 2 junction combinations with at least 2 reads each
+  message("working with genes with at least 2 junctions expressed with more than 2 read counts")
+  junCombCounts <-
+    left_join(
+      junCombCounts,
+      readsToJunctionsCollapse %>%
+        ungroup() %>%
+        distinct(juncComb, .keep_all = TRUE) %>%
+        dplyr::select(juncComb, gene_id),
+      by = "juncComb"
+    )
+  genesWithMoreThan1JunExpressed <- junCombCounts %>%
+    filter(n >= 2) %>%
+    group_by(gene_id) %>%
+    tally() %>%
+    filter(n > 1)
+  message("genesToWork:", nrow(genesWithMoreThan1JunExpressed))
+  # lets start with tss
+  # make pairs ids for junction-tss combinations
+  testSub <-
+    readsToJunctions %>%
+    filter(gene_id %in% genesWithMoreThan1JunExpressed$gene_id) %>%
+    mutate(
+      tssJunID = paste0(promoter_id, ":", new_junID),
+      isoID = paste0(promoter_id, ":", new_junID, ":", tes_id)
+    )
+  cat("test")
+  # lets make small database to annotate the counts
+  simpleT <-
+    testSub %>%
+    dplyr::select(tes_id, promoter_id, new_junID, tssJunID, isoID) %>%
+    distinct(isoID, .keep_all = TRUE)
+  ### summarize junctions
+  # counts per pair
+  tssJunCounts <- testSub %>%
+    group_by(tssJunID) %>%
+    tally()
+  tssJunCounts <-
+    left_join(
+      tssJunCounts,
+      simpleT %>% distinct(tssJunID, .keep_all = TRUE) %>% dplyr::select(tssJunID, promoter_id, new_junID),
+      by = "tssJunID"
+    ) %>% mutate(gene_id = gsub("\\:.*", "", .data$promoter_id) )
+  annotPairsExp <- tssJunCounts
+  # make table of junction fractions
+  annotPairsExp <-
+    annotPairsExp %>% dplyr::group_by(new_junID) %>% dplyr::mutate(jun_sum = sum(n)) %>%
+    group_by(promoter_id) %>% dplyr::mutate(start_sum = sum(n)) %>% dplyr::group_by(tssJunID) %>% dplyr::mutate(pairs_sum = sum(n)) %>%
+    group_by(gene_id) %>% mutate(geneMean = sum(n))
+  # FilterGenes with more than one TSS
+  genesATSS <-
+    annotPairsExp %>% dplyr::distinct(promoter_id, .keep_all = TRUE) %>% dplyr::group_by(gene_id) %>% dplyr::tally() %>% dplyr::filter(n >1) %>% dplyr::pull(gene_id)
+  annotPairsExp <- annotPairsExp %>% dplyr::filter(gene_id %in% genesATSS)
+  # Keep only genes with mutiple junction combinations
+  genesMJC <-
+    annotPairsExp %>% dplyr::distinct(new_junID, .keep_all = TRUE) %>% dplyr::group_by(gene_id) %>% tally() %>% filter(n >1) %>% dplyr::pull(gene_id)
+  annotPairsExp <- annotPairsExp %>% filter(gene_id %in% genesMJC)
+  ### Testing
+  ##########################################################################################
+  perGeneList <- split(annotPairsExp, f = annotPairsExp$gene_id)
+  couplingsmatrix <- lapply(perGeneList, function(x) {
+    x1 <-
+      x %>% maditr::dcast(new_junID ~ promoter_id, value.var = "n") %>% as.data.frame(.)
+    x1 <- na.omit(x1)
+    if (nrow(x1) > 0) {
+      rownames(x1) <- x1$new_junID
+      x1$new_junID <- NULL
+      return(x1)
+    } else{
+      return(x1)
+    }
+  })
+  couplingsmatrix <- Filter(function(x)
+    nrow(x) > 1, couplingsmatrix)
+  couplingsmatrix <-
+    lapply(couplingsmatrix, function(x) {
+      x[is.na(x)] <- 0
+      x
+    })
+  couplingsmatrix <- lapply(couplingsmatrix, function(x) {
+    x + 1
+  })
+  ## fixes remove single junction genes.
+  perGeneChisqTest <- lapply(couplingsmatrix, function(x) {
+    # MONTE CARLO SIMULATION P-VALUE
+    x2 <- chisq.test(x, simulate.p.value = TRUE)
+    x1 <- x2$p.value
+    return(x1)
+  })
+
+  perGeneSquareRes <- lapply(couplingsmatrix, function(x) {
+    x2 <- chisq.test(x, simulate.p.value = TRUE)
+    # RESIDUALS from chisq.test
+    x1 <- x2$statistic
+    return(x1)
+  })
+
+  makeDFPerGene <- function(x){
+    td <- chisq.test(x)
+    tdo <- as.data.frame(td$observed) %>%
+      dplyr::mutate(new_junID=rownames(.))
+    tdo <- reshape2::melt( tdo) %>%
+      mutate(pairs_id = paste0(new_junID,":",variable)) %>%
+      dplyr::select(pairs_id, value) %>%
+      dplyr::rename(observed=value)
+    tde <- as.data.frame(td$expected) %>%
+      dplyr::mutate(new_junID=rownames(.))
+    tde <- reshape2::melt( tde) %>%
+      mutate(pairs_id = paste0(new_junID,":",variable)) %>%
+      dplyr::select(pairs_id, value) %>%
+      dplyr::rename(expected=value)
+    tdr <- as.data.frame(td$residuals) %>%
+      dplyr::mutate(new_junID=rownames(.))
+    tdr <- reshape2::melt( tdr) %>%
+      dplyr::mutate(pairs_id = paste0(new_junID,":",variable),
+                    gene_id=gsub("\\:.*","",new_junID) ) %>%
+      dplyr::select(pairs_id, value, gene_id) %>%
+      dplyr::rename(residuals=value)
+    d<- left_join(tdo, tde, by="pairs_id")
+    d<- left_join(d, tdr, by="pairs_id")
+    return(d)
+  }
+  x2 <-  lapply( couplingsmatrix, makeDFPerGene)
+  x3 <- do.call(rbind,x2)
+  longTable <- x3
+  tt2 <- test2 %>%
+    dplyr::select(new_junID,jun_id) %>%
+    distinct(new_junID, .keep_all = TRUE) %>%
+    dplyr::rename(gene_id=new_junID)
+  longTable <- left_join(longTable,  tt2, by="gene_id")
+  affectedGenes <- as.data.frame(do.call(rbind, perGeneChisqTest)) %>%
+    dplyr::rename(p.value.chisq = 1) %>%
+    dplyr::mutate(gene_id = rownames(.))
+  affectedGenesXsquare <- as.data.frame(do.call(rbind, perGeneSquareRes)) %>%
+    dplyr::rename(sqrtRes = 1) %>%
+    dplyr::mutate(gene_id = rownames(.))
+  affectedGenes<- left_join(affectedGenes, affectedGenesXsquare, by="gene_id")
+  affectedGenes$adj.pvalue <- p.adjust(affectedGenes$p.value.chisq)
+  resultTest  <- list()
+  resultTest$couplingsPerGene  <- affectedGenes
+  resultTest$couplingsPerJunction  <- longTable
+  return(resultTest)
+}
---
+++ R/create_reference_junctions.R
@@ -0,0 +1,189 @@
+#' Load short read sjb data
+#'
+#' @param sjbPath
+#' @importFrom dplyr %>%
+#' @importFrom dplyr mutate
+#' @importFrom dplyr filter
+#' @importFrom rlang .data
+#' @return
+#' @export
+#'
+#' @examples
+read_sjout <- function(sjbPath) {
+  junctionFile <- readr::read_table(sjbPath, col_names = FALSE)
+  colnames(junctionFile) <-
+    c(
+      "seqnames",
+      "start",
+      "end",
+      "strand",
+      "intronMotif",
+      "comp.ref",
+      "read.count",
+      "multimappers",
+      "overhang"
+    )
+
+  junctionFile <- junctionFile %>%
+    dplyr::mutate(
+      juncID = paste0(.data$seqnames, ":", .data$start, "-", .data$end),
+      strand = dplyr::case_when(
+        .data$strand == 0 ~ "*",
+        .data$strand == 1 ~ "+",
+        TRUE ~ "-"
+      )
+    )
+  return(junctionFile)
+}
+#' Extract junctions from annotation
+#'
+#' @param refAnnot
+#' @importFrom rlang .data
+#' @return
+#' @export
+#'
+#' @examples
+make_annotation_txf <- function(refAnnot){
+  txdb <- GenomicFeatures::makeTxDbFromGRanges(refAnnot)
+  annot_junctions <- SGSeq::convertToTxFeatures(txdb)
+  annot_junctions <- annot_junctions[SGSeq::type(annot_junctions) == "J"]
+  GenomicRanges::start(annot_junctions) <- GenomicRanges::start(annot_junctions) + 1
+  GenomicRanges::end(annot_junctions) <- GenomicRanges::end(annot_junctions) - 1
+  return(annot_junctions)
+}
+
+#' Annotate junctions
+#'
+#' @param AnnotJunctions
+#' @param refAnnot
+#' @param linksDatabase
+#' @importFrom rlang .data
+#' @return
+#' @export
+#'
+#' @examples
+get_junctions_from_annot <- function(AnnotJunctions, refAnnot,linksDatabase,genesMultipleJunctions){
+  # overlap junctions to windows to filter
+  message("Removing junctions overlapping TSS and 3'end windows")
+  junctionsInPromoters <- GenomicRanges::findOverlaps(AnnotJunctions, linksDatabase$promoterDependentWindow)
+  juncNotIn5prime <- AnnotJunctions[-S4Vectors::queryHits(junctionsInPromoters)]
+  junctionsIn3ends <- GenomicRanges::findOverlaps(AnnotJunctions, linksDatabase$tesDependentWindow)
+  juncNotIn3prime <- AnnotJunctions[-S4Vectors::queryHits(junctionsIn3ends)]
+  junctionsToWork <- unique( c(juncNotIn5prime,juncNotIn3prime))
+  # keep only genes with more than one junction combinations
+  message("Removing genes with more")
+  # remove single junction genes
+  message("Removing single CDS isoform genes")
+  junctionsToWork <- junctionsToWork %>%
+    as.data.frame(.data) %>% tidyr::unnest(.data$geneName) %>%
+    dplyr::filter(.data$geneName %in% genesMultipleJunctions$geneName) %>%
+    GenomicRanges::makeGRangesFromDataFrame(.data, keep.extra.columns = TRUE)
+  junctionsToWork <- unique(junctionsToWork)
+  junctionsToWork$txName <- NULL
+  junctionsToWork$juncID <-
+    paste0( GenomicRanges::seqnames(junctionsToWork),
+           ":",
+           GenomicRanges::start(junctionsToWork),
+           "-",
+           GenomicRanges::end(junctionsToWork))
+  GenomicRanges::mcols(junctionsToWork) <-  GenomicRanges::mcols(junctionsToWork)[c(2,3)]
+  junctionRef <- junctionsToWork
+  junctionRef$gene_id <- junctionRef$geneName
+  junctionRef$geneName <- NULL
+  return(junctionRef)
+}
+
+#' Get junctions from short read sequencing data
+#'
+#' @param pathSJ.out
+#' @param min.counts
+#' @param AnnotJunctions
+#' @param refAnnot
+#' @param linksDatabase
+#' @importFrom rlang .data
+#' @return
+#' @export
+#'
+#' @examples
+get_junctions_from_short_reads <- function(pathSJ.out, min.counts,AnnotJunctions,refAnnot,linksDatabase,genesMultipleJunctions){
+  message("Load splice junction files")
+  junctionCounts <- read_sjout(pathSJ.out)
+  junctionCounts <- junctionCounts %>%
+    dplyr::filter(.data$read.count > min.counts) %>%
+    dplyr::select(1, 2, 3, 4, 7, 10) %>%
+    GenomicRanges::makeGRangesFromDataFrame(.data, keep.extra.columns = TRUE)
+  # overlap junctions to windows to filter
+  junctionsInPromoters <- GenomicRanges::findOverlaps(junctionCounts, linksDatabase$promoterDependentWindow)
+  juncNotIn5prime <- junctionCounts[-S4Vectors::queryHits(junctionsInPromoters)]
+  junctionsIn3ends <- GenomicRanges::findOverlaps(junctionCounts, linksDatabase$tesDependentWindow)
+  juncNotIn3prime <- junctionCounts[-S4Vectors::queryHits(junctionsIn3ends)]
+  shortReadJunctions <- unique( c(juncNotIn5prime,juncNotIn3prime))
+  # annotate junctions to genes
+  geneBins <- unique(unlist( range( split(refAnnot, f=refAnnot$gene_id) )))
+  hits2Genes <- GenomicRanges::findOverlaps(  shortReadJunctions, geneBins, type = "within")
+  shortReadJunctions <- shortReadJunctions[S4Vectors::queryHits(hits2Genes),]
+  shortReadJunctions$gene_id <- names(geneBins[S4Vectors::subjectHits(hits2Genes),])
+  GenomicRanges::mcols(shortReadJunctions)<- GenomicRanges::mcols(shortReadJunctions)[c("gene_id", "juncID")]
+  shortReadJunctions <-  unique(shortReadJunctions)
+  # keep only genes with more than one junction combinations
+  message("single isoform genes")
+  shortReadJunctions <- shortReadJunctions[shortReadJunctions$gene_id %in% genesMultipleJunctions$geneName,]
+  return(shortReadJunctions)
+}
+#' Title
+#'
+#' @param pathSJ.out
+#' @param min.jcounts
+#' @param AnnotJunctions
+#' @param refAnnot
+#' @param type
+#' @importFrom rlang .data
+#' @return
+#' @export
+#'
+#' @examples
+create_reference_junctions <- function(pathSJ.out, min.jcounts,AnnotJunctions, refAnnot, type){
+  message("Preparing Annotation of 5'and 3' ends")
+  linksDatabase <- prepareLinksDatabase(refAnnot,
+                                              tss.window = 50,
+                                              tes.window = 150)
+  # window of exons dependent on ATSS usage
+  linksDatabase$promoterDependentWindow <-
+    unlist( range(
+      split(
+        linksDatabase$TSSCoordinate.base,
+        f = linksDatabase$TSSCoordinate.base$value.group_name
+      )
+    ))
+
+  # window of exons dependent on alternative 3'end usage
+  linksDatabase$tesDependentWindow <-
+    unlist( range(
+      split(
+        linksDatabase$TESCoordinate.base,
+        f = linksDatabase$TESCoordinate.base$value.group_name
+      )
+    ))
+
+  # get genes with more 1 different junction
+  genesMultipleJunctions <- AnnotJunctions %>% as.data.frame(.data) %>%
+    tidyr::unnest(.data, .data$txName) %>% tidyr::unnest(.data, .data$geneName) %>%
+    mutate(JunID = paste0(.data$seqnames, .data$start, .data$end)) %>%
+    dplyr::group_by(.data$txName) %>%
+    dplyr::mutate(junName = paste0(.data$JunID, collapse = "|")) %>%
+    dplyr::distinct(.data$junName, .keep_all = TRUE) %>%
+    dplyr::group_by(.data$geneName) %>% dplyr::tally() %>% dplyr::filter(.data$n>1)
+
+  if(type=="short"){
+    shortReadJunctions <- get_junctions_from_short_reads(pathSJ.out,
+                                                         min.counts=min.jcounts,
+                                                         AnnotJunctions,
+                                                         refAnnot )
+    annotationJunctions <- get_junctions_from_annot(AnnotJunctions, refAnnot)
+    reference_junctions <- c(shortReadJunctions, annotationJunctions)
+    return(reference_junctions)
+  }else{
+    annotation_junctions <- get_junctions_from_annot(AnnotJunctions, refAnnot)
+    return(annotation_junctions)
+  }
+}
